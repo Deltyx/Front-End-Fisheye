19fa77c05b1ad46a62cca4d95355cecd96cd21dc
Alexis Morin <95691178+Deltyx@users.noreply.github.com>
2022-02-13 18:26:19 +0100

    Add files via upload

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,41 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const pht = document.createElement( 'h2' );
+        const loc = document.createElement( 'h3' );
+        const tag = document.createElement( 'h3' );
+        const prc = document.createElement( 'h3' );
+        const link = document.createElement( 'a' );
+
+        img.setAttribute("src", picture);
+        link.setAttribute("href", "photographer.html"+`?id=${id}`);
+
+        article.classList.add("profile_article");
+        img.classList.add("profile_img");
+        pht.classList.add("profile_name");
+        loc.classList.add("profile_location");
+        tag.classList.add("profile_tagline");
+        prc.classList.add("profile_price");
+        link.classList.add("profile_link");
+
+        pht.textContent = name;
+        loc.textContent = city + ", " + country;
+        tag.textContent = tagline;
+        prc.textContent = price + "€/jour";
+
+        article.appendChild(link);
+        link.appendChild(img);
+        article.appendChild(pht);
+        article.appendChild(loc);
+        article.appendChild(tag);
+        article.appendChild(prc);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, picture, city, country, tagline, price, id, getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,7 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+async function displayCurrentProfile()
+{
+    let url = document.URL;
+    let id = url.substring(url.lastIndexOf('=') + 1);
+}
