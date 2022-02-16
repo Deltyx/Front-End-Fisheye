c10fe27437b365ac40c8d5e43346abe1e677d703
Deltyx <alexismorin.pro@gmail.com>
2022-02-17 00:02:28 +0100

    Added Init() W.I.P

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -4,17 +4,23 @@ function getId() {
         { get: (searchParams, prop) => searchParams.get(prop) }).id;  
 }
 
-
+function displayProfile(profile) {
+    
+}
 
 async function init() {
 
     const id = getId();
-    const { data } = await getData();
-    //const medias = data.filter(data => data.photographerId == id);
+    const data = await getData();
+    const medias = data.media.filter(data => data.photographerId == id);
+    const profile = data.photographers.filter(data => data.id == id);
+    const Profile = new Photographer(profile); 
 
     console.log(id);
     console.log(data);
-    //console.log(medias);
+    console.log(medias);
+    console.log(profile);
+    console.log(Profile);
 };
 
 init();
